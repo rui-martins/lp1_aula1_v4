c6ff121e18f054c8845ee6fa8827dba23eb50df7
rui-martins <vandroste@gmail.com>
2018-02-15 17:59:41 +0000

    exercicio6 - areas

---
+++ Cilindro/Cilindro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.1415f;
+            double height;
+            double radius;
+            double area;
+            double volume;
+
+            Console.WriteLine("\nInsert Height:\n");
+            height = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\nInsert Radius:\n");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            area = (2 * pi * (radius *(radius + height)));
+            volume = pi * (radius * radius) * height;
+
+            Console.WriteLine("\nArea:\n" + area);
+            Console.WriteLine("\nVolume:\n" + volume);
+
+
+
+        }
+    }
+}
---
+++ Cilindro/Cilindro/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cilindro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cilindro")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ca51093-bf27-4b2f-9753-97113c5d59ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
